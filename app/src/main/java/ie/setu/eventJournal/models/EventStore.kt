5b67ee5018c6c31bf067846d61adb86335dd1a5d
emmaroche <20088680@mail.wit.ie>
2024-04-30 16:50:48 +0100

    Add option to toggle events by favourite

--- app/src/main/java/ie/setu/eventJournal/models/EventStore.kt
+++ app/src/main/java/ie/setu/eventJournal/models/EventStore.kt
@@ -4,14 +4,25 @@ import androidx.lifecycle.MutableLiveData
 import com.google.firebase.auth.FirebaseUser
 
 interface EventStore {
-    fun findAll(eventsList:
-                MutableLiveData<List<EventModel>>)
-    fun findAll(userid:String,
-                eventsList:
-                MutableLiveData<List<EventModel>>)
-    fun findById(userid:String, eventid: String,
-                 event: MutableLiveData<EventModel>)
+    fun findAll(
+        eventsList:
+        MutableLiveData<List<EventModel>>
+    )
+    fun findAll(
+        userid: String,
+        eventsList:
+        MutableLiveData<List<EventModel>>
+    )
+    fun findAllFavourites(
+        userid: String,
+        eventsList:
+        MutableLiveData<List<EventModel>>)
+    fun findById(
+        userid: String, eventid: String,
+        event: MutableLiveData<EventModel>
+    )
     fun create(firebaseUser: MutableLiveData<FirebaseUser>, event: EventModel)
-    fun delete(userid:String, eventid: String)
-    fun update(userid:String, eventid: String, event: EventModel)
+    fun delete(userid: String, eventid: String)
+    fun deleteAllEvents(userid: String)
+    fun update(userid: String, eventid: String, event: EventModel)
 }
