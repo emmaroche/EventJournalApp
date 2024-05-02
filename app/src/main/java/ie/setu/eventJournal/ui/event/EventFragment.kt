09d93e59fd4e111bdeef3dbdfbaf96dbb2d92c3c
emmaroche <20088680@mail.wit.ie>
2024-05-02 18:47:29 +0100

    Update ui and ux of app bug fixes

--- app/src/main/java/ie/setu/eventJournal/ui/event/EventFragment.kt
+++ app/src/main/java/ie/setu/eventJournal/ui/event/EventFragment.kt
@@ -1,11 +1,20 @@
 package ie.setu.eventJournal.ui.event
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
-import android.view.*
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
@@ -28,8 +37,10 @@ import ie.setu.eventJournal.models.EventModel
 import ie.setu.eventJournal.ui.auth.LoggedInViewModel
 import ie.setu.eventJournal.ui.map.MapsViewModel
 import ie.setu.eventJournal.ui.report.ReportViewModel
+import ie.setu.eventJournal.utils.hideSoftKeyboard
 import ie.setu.eventJournal.utils.showImagePicker
 import timber.log.Timber
+import java.util.Calendar
 
 class EventFragment : Fragment() {
 
@@ -47,6 +58,11 @@ class EventFragment : Fragment() {
         _fragBinding = FragmentEventBinding.inflate(inflater, container, false)
         val root = fragBinding.root
 
+        root.setOnTouchListener { _, _ ->
+            hideSoftKeyboard()
+            false
+        }
+
         setupMenu()
         eventViewModel = ViewModelProvider(this).get(EventViewModel::class.java)
         eventViewModel.observableStatus.observe(viewLifecycleOwner, Observer { status ->
@@ -61,14 +77,62 @@ class EventFragment : Fragment() {
 
         registerImagePickerCallback()
 
+        // Reference used to help with adding the date/calendar picker: https://stackoverflow.com/questions/51490354/datepicker-in-kotlin-showing-wrong-month
+        fragBinding.editTextDate.setOnClickListener {
+           hideSoftKeyboard()
+            val calendar = Calendar.getInstance()
+            val year = calendar.get(Calendar.YEAR)
+            val month = calendar.get(Calendar.MONTH)
+            val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+            DatePickerDialog(requireContext(), { _, pickedYear, pickedMonth, pickedDay ->
+                val pickedDate = "${pickedDay}/${pickedMonth + 1}/${pickedYear}"
+                fragBinding.editTextDate.setText(pickedDate)
+            }, year, month, day).show()
+        }
+
+        // Resource used to help add drop down options for event types: https://www.geeksforgeeks.org/spinner-in-kotlin/
+        val spinner = fragBinding.spinnerEventType
+        val eventTypes = resources.getStringArray(R.array.eventTypesArray)
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, eventTypes)
+        spinner.setAdapter(adapter)
+        spinner.setOnClickListener {
+            hideSoftKeyboard()
+        }
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+
+            override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+                Toast.makeText(requireContext(), getString(R.string.event_type) + " " + eventTypes[position], Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                // Nothing
+            }
+        }
+
+        // Reference used to help with adding the time picker: https://devendrac706.medium.com/timepickerdialog-android-studio-tutorial-time-picker-using-kotlin-in-android-studio-f9949732d62
+        fragBinding.editTextTime.setOnClickListener {
+            hideSoftKeyboard()
+            val calendar = Calendar.getInstance()
+            val hour = calendar.get(Calendar.HOUR_OF_DAY)
+            val minute = calendar.get(Calendar.MINUTE)
+
+            TimePickerDialog(requireContext(), { _, pickedHour, pickedMinute ->
+                val pickedTime = String.format("%02d:%02d", pickedHour, pickedMinute)
+                fragBinding.editTextTime.setText(pickedTime)
+            }, hour, minute, true).show()
+        }
+
         fragBinding.eventButton.setOnClickListener {
+            hideSoftKeyboard()
             val amount = if (fragBinding.paymentAmount.text.isNotEmpty())
-                fragBinding.paymentAmount.text.toString().toInt() else TODO()
+                fragBinding.paymentAmount.text.toString().toInt() else 0
             val eventName = fragBinding.editTextName.text.toString()
             val eventTime = fragBinding.editTextTime.text.toString()
             val eventDate = fragBinding.editTextDate.text.toString()
             val eventDescription = fragBinding.editTextDescription.text.toString()
-            val eventType = fragBinding.editTextType.text.toString()
+            val eventType = fragBinding.spinnerEventType.text.toString()
 
             if (totalBudget >= fragBinding.progressBar.max)
                 Toast.makeText(context, "Amount Exceeded!", Toast.LENGTH_LONG).show()
@@ -183,7 +247,6 @@ class EventFragment : Fragment() {
         val inputStream = requireContext().contentResolver.openInputStream(uri)
         return BitmapFactory.decodeStream(inputStream)
     }
-
 
     fun doSelectImage() {
         showImagePicker(imageIntentLauncher, view)
